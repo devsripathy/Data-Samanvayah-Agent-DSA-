2665f839c0c9be55e1107b6af4e9c370e699dd83
devsripathy <dev.sripathy@gmail.com>
2026-07-09 11:08:34 +0530

    Add logging configuration utility

---
+++ src/utils/logger.py
@@ -0,0 +1,17 @@
+"""Logging configuration for DSA."""
+import logging
+import sys
+
+def get_logger(name: str) -> logging.Logger:
+    """Returns a configured logger instance."""
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter(
+            '%(asctime)s | %(levelname)-8s | %(name)s | %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+    return logger
