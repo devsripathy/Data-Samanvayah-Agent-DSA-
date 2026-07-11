5904edcd15aad00c9a5da1924d2437fcc1ba90bb
devsripathy <dev.sripathy@gmail.com>
2026-07-11 14:16:29 +0530

    Update node.py

--- src/agents/supervisor/node.py
+++ src/agents/supervisor/node.py
@@ -1,6 +1,8 @@
 """Supervisor agent node."""
 from src.core.state import DSAState
 from src.utils.logger import get_logger
+from typing_extensions import TypedDict
+
 
 logger = get_logger(__name__)
 
