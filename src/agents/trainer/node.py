e2cda2acf760369f0e3ba90e59582ffeddebb7c8
devsripathy <dev.sripathy@gmail.com>
2026-07-11 14:17:23 +0530

    Refactor trainer_node with TypedDict for results

--- src/agents/trainer/node.py
+++ src/agents/trainer/node.py
@@ -1,6 +1,7 @@
 """Trainer agent node for model training."""
 from src.core.state import DSAState, TrainingResults
 from src.utils.logger import get_logger
+from typing_extensions import TypedDict
 import time
 
 logger = get_logger(__name__)
